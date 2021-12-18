dde768f8ffe48c98f2b84f4ea969344f8dafed7b
lllgts <wuer031@126.com>
2021-12-18 21:34:29 +0800

    Revert "binder: Import from android11-5.4-lts branch at 5fbf84689f11"
    This reverts commit 3e6d77736e0e3e5297cbcb78439e990759daffa9.

--- include/uapi/linux/android/binder.h
+++ include/uapi/linux/android/binder.h
@@ -1,4 +1,3 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
  * Copyright (C) 2008 Google, Inc.
  *
@@ -67,7 +66,6 @@ enum flat_binder_object_flags {
 	 * @FLAT_BINDER_FLAG_ACCEPTS_FDS: whether the node accepts fds.
 	 */
 	FLAT_BINDER_FLAG_ACCEPTS_FDS = 0x100,
-
 	/**
 	 * @FLAT_BINDER_FLAG_SCHED_POLICY_MASK: bit-mask for scheduling policy
 	 *
@@ -296,7 +294,6 @@ enum transaction_flags {
 	TF_ROOT_OBJECT	= 0x04,	/* contents are the component's root object */
 	TF_STATUS_CODE	= 0x08,	/* contents are a 32-bit status code */
 	TF_ACCEPT_FDS	= 0x10,	/* allow replies with file descriptors */
-	TF_CLEAR_BUF	= 0x20,	/* clear buffer on txn complete */
 };
 
 struct binder_transaction_data {
