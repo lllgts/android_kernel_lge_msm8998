460bae6e3fda59fcb54ae7f1875e86ed3159d766
lllgts <wuer031@126.com>
2022-01-01 00:16:36 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ args="-j$(nproc --all) \
 	O=out \
 	ARCH=arm64 \
 	CLANG_TRIPLE=aarch64-linux-gnu- \
-	CROSS_COMPILE=~/gcc4.9-gun/bin/aarch64-linux-android- \
-	CC=~/clang14/clang-r437112/bin/clang-14 \
-	CROSS_COMPILE_ARM32=~/gcc4.9-eabi/bin/arm-linux-androideabi- "
+	CROSS_COMPILE=/home/lllgts/gcc4.9-gun/bin/aarch64-linux-android- \
+	CC=/home/lllgts/clang14/clang-r437112/bin/clang-14 \
+	CROSS_COMPILE_ARM32=/home/lllgts/gcc4.9-eabi/bin/arm-linux-androideabi- "
 	make ${args} lineageos_h930_defconfig
 	make ${args}
