d63375ca6b15d774263ada8ece071ab43cdd3198
lllgts <wuer031@126.com>
2022-05-01 02:13:53 +0000

    vdso32: Remove -no-integrated-as *kang from https://github.com/aled99/apollo/commit/bf72eb55844232ce77dc7b5d0f65b78b28eaa3dd

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ args="-j$(nproc --all) \
 	O=out \
 	ARCH=arm64 \
 	CLANG_TRIPLE=aarch64-linux-gnu- \
-	CROSS_COMPILE=/home/lllgts/gcc4.9-gun/bin/aarch64-linux-android- \
-	CC=/home/lllgts/clang14/clang-r437112/bin/clang-14 \
-	CROSS_COMPILE_ARM32=/home/lllgts/gcc4.9-eabi/bin/arm-linux-androideabi- "
+	CROSS_COMPILE=/home/lllgts/clang13/bin/aarch64-linux-gnu- \
+	CC=/home/lllgts/clang/clang-r445002/bin/clang \
+	NM=/home/lllgts/clang/clang-r445002/bin/llvm-nm \
+	CROSS_COMPILE_ARM32=/home/lllgts/clang13/bin/arm-linux-gnueabi- "
 	make ${args} lineageos_h930_defconfig
 	make ${args}
