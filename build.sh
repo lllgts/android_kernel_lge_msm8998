16a22933764773f1f7d4d04c5ff1d32e71e7d045
lllgts <wuer031@126.com>
2023-07-17 07:15:09 +0000

    build: Use Google Clang r450784e

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ args="-j$(nproc --all) \
 	ARCH=arm64 \
 	CLANG_TRIPLE=aarch64-linux-gnu- \
 	CROSS_COMPILE=/home/lllgts/clang13/bin/aarch64-linux-gnu- \
-	CC=/home/lllgts/clang/clang-r445002/bin/clang \
-	NM=/home/lllgts/clang/clang-r445002/bin/llvm-nm \
+	CC=/home/lllgts/clang/clang-r450784e/bin/clang \
+	NM=/home/lllgts/clang/clang-r450784e/bin/llvm-nm \
 	CROSS_COMPILE_ARM32=/home/lllgts/clang13/bin/arm-linux-gnueabi- "
 	make ${args} lineageos_h930_defconfig
 	make ${args}
